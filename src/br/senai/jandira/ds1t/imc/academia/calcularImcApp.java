b2a52fc3d55da35e7547f33004f081a0b4a7865c
JEFFERSON VICTOR NEVES DA SILVA <25204074@cfp127aluno.local>
2025-09-22 16:58:21 -0300

    subindo o codigo pro git

--- src/br/senai/jandira/ds1t/imc/academia/calcularImcApp.java
+++ src/br/senai/jandira/ds1t/imc/academia/calcularImcApp.java
@@ -1,3 +1,5 @@
+package br.senai.jandira.ds1t.imc.academia;
+
 public class calcularImcApp {
     public static void main(String[] args) {
         System.out.println("Academia SENAI");
--- src/br/senai/jandira/ds1t/imc/academia/model/calcularImc.java
+++ src/br/senai/jandira/ds1t/imc/academia/model/calcularImc.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+package br.senai.jandira.ds1t.imc.academia.model;
+
+import java.util.Scanner;
 
 public class calcularImc {
+
+    String nomeAluno;
+    String condição;
+    double pesoDoAluno;
+    double alturaDoAluno;
+    double imcDoAluno;
+
+    public void obterDados(){
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("DIGITE O NOME DO ALUNO: ");
+        nomeAluno = leitor.nextLine();
+
+        System.out.println("DIGITE O PESO DO ALUNO: ");
+        pesoDoAluno = leitor.nextDouble();
+
+        System.out.println("DIGITE A ALTURA DO ALUNO: ");
+        pesoDoAluno = leitor.nextDouble();
+
+
+    }
+
+    public void calcularImcDoAluno{
+
+
+    }
+
+
 }
